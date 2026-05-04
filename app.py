22d945889a33618805cefa6bc7f00967018aecb4
nitambiswas2 <nitambiswas2026@gmail.com>
2026-05-04 20:29:31 +0530

    correction of db

--- app.py
+++ app.py
@@ -15,12 +15,20 @@ app = Flask(__name__)
 #    autocommit=True
 # )
 
+# db = mysql.connector.connect(
+#    host=os.getenv("DB_HOST"),
+#    user=os.getenv("DB_USER"),
+#    password=os.getenv("DB_PASSWORD"),
+#    database=os.getenv("DB_NAME"),
+#    port=int(os.getenv("DB_PORT", 3306))
+# )
+
 db = mysql.connector.connect(
-   host=os.getenv("DB_HOST"),
-   user=os.getenv("DB_USER"),
-   password=os.getenv("DB_PASSWORD"),
-   database=os.getenv("DB_NAME"),
-   port=int(os.getenv("DB_PORT", 3306))
+    host=os.getenv("MYSQLHOST"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE"),
+    port=int(os.getenv("MYSQLPORT"))
 )
 
 app.secret_key = os.getenv("SECRET_KEY")
